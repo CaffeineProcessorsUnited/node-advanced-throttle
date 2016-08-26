35cc56178261c78cb6c08ec9181252d2ec55d31a
Malte Heinzelmann <malte@hnzlmnn.de>
2016-08-26 18:16:13 +0200

    Renamed event

--- throttle.js
+++ throttle.js
@@ -70,7 +70,7 @@ Throttle.prototype.destroy = function() {
 Throttle.prototype._passthroughChunk = function () {
   this._passthrough(this.chunkSize, this._onchunk);
   this.totalBytes += this.chunkSize;
-  this.emit("playback_time")
+  this.emit("timechanged", this.WrittenTime());
 };
 
 Throttle.prototype._onchunk = function (output, done) {
